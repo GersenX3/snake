b915fb08b14b487fd188921579bf970585d365c0
Gerson Lopez <galdl1438@gmail.com>
2022-07-18 00:53:05 -0500

    Se agregaro el titulo como imagen
    se agregaron los scripts de la viborita y se esta tratando de imprimir
    la primer imagen en el canvas

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var can = document.getElementById("canvasH");
 var lienzo = can.getContext("2d");
-
 //Dibujar
 function dibujar(xInicial,yInicial,xFinal,yFinal,ancho)
 {
 lienzo.beginPath();
 lienzo.moveTo(xInicial,yInicial);
 lienzo.lineTo(xFinal,yFinal);
-lienzo.strokeStyle = "darkgreen";
+lienzo.strokeStyle = "#112b0f";
 lienzo.lineWidth = ancho;
 lienzo.stroke();
 lienzo.closePath();
@@ -17,3 +16,27 @@ dibujar(5,5,5,515,10);
 dibujar(0,5,515,5,10);
 dibujar(0,515,515,515,10);
 dibujar(515,0,515,520,10);
+
+//agregando sprites
+var sprites = {up:"/sources/vHeadUp.png",down:"/sources/vHeadDown.png",left:"/sources/vHeadLeft.png",right:"/sources/vHeadRight.png",
+tailUp:"/sources/vTaleUp.png",tailDown:"/sources/vTaleDown.png",tailLeft:"/sources/vTaleLeft.png",tailRight:"/sources/vTaleRight.png",}
+var snake = {src : sprites, loaded: false,};
+
+snake.imagen = new Image();
+snake.imagen.src = snake.src;
+snake.imagen.addEventListener("load",loadedSnake)
+// Funcionas lanzadas al cargar imagenes
+function loadedSnake()
+{
+    snake.loaded = true;
+    draw()
+}
+
+function draw()
+{
+    if(snake.loaded)
+    {
+        lienzo.drawImage(snake.src.up,235,235)
+    }
+}
+draw()
